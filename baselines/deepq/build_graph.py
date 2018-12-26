48fe3097ab6a6794ceda3f2879ab7fb8062b3770
lishuailong <lishuailong@sia.cn>
2018-12-26 10:57:58 +0800

    Update build_graph.py

--- baselines/deepq/build_graph.py
+++ baselines/deepq/build_graph.py
@@ -393,15 +393,15 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, grad_norm_clipping=
         target_q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=tf.get_variable_scope().name + "/target_q_func")
 
         # q scores for actions which we know were selected in the given state.
-        q_t_selected = tf.reduce_sum(q_t * tf.one_hot(act_t_ph, num_actions), 1)
+        q_t_selected = tf.reduce_sum(q_t * tf.one_hot(act_t_ph, num_actions), 1)           #先one-hot编码，列上求和
 
         # compute estimate of best possible value starting from state at t + 1
         if double_q:
             q_tp1_using_online_net = q_func(obs_tp1_input.get(), num_actions, scope="q_func", reuse=True)
-            q_tp1_best_using_online_net = tf.argmax(q_tp1_using_online_net, 1)
+            q_tp1_best_using_online_net = tf.argmax(q_tp1_using_online_net, 1)               #返回每一行最大值索引
             q_tp1_best = tf.reduce_sum(q_tp1 * tf.one_hot(q_tp1_best_using_online_net, num_actions), 1)
         else:
-            q_tp1_best = tf.reduce_max(q_tp1, 1)
+            q_tp1_best = tf.reduce_max(q_tp1, 1)                                             #找到并返回行上最大值
         q_tp1_best_masked = (1.0 - done_mask_ph) * q_tp1_best
 
         # compute RHS of bellman equation
